a5a4945fc84f4453ff8b1a8dec09fde5f41cae6d
Vijaykrish0807 <94906739+Vijaykrish0807@users.noreply.github.com>
2022-06-04 14:29:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Home from "./pages/Home";
+const App = () => {
+  return <Home></Home>;
+};
+
+export default App;
---
+++ src/sliderItems.js
@@ -0,0 +1,28 @@
+ //import React from "react";
+ const sliderItems =[
+    {
+        id: 1,
+        img: "https://data1.ibtimes.co.in/photo/en/full/109830/samantha-looks-hot-barely-there-dress.jpg?w=900",
+        title: "Summer Sale",
+        desc: "DON'T cOMPROMISE ON STYLE!!!! XYX....",
+        bg:"f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://data1.ibtimes.co.in/photo/en/full/109830/samantha-looks-hot-barely-there-dress.jpg?w=900",
+        title: "Wintter Sale",
+        desc: "DON'T cOMPROMISE ON STYLE!!!! XYX....",
+        bg:"fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://data1.ibtimes.co.in/photo/en/full/109830/samantha-looks-hot-barely-there-dress.jpg?w=900",
+        title: "Seasoal Sale",
+        desc: "DON'T cOMPROMISE ON STYLE!!!! XYX....",
+        bg:"fbf0f4",
+    }
+
+];
+export default sliderItems;
+
+
